04842a6b157b59601ff370edb905012eb17b0cd3
King <giovani.miantsoko@gmail.com>
2023-07-19 15:02:24 +0200

    init

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Bonjour, world Devops !");
+        Additionner(1, 2);
+    }
+    public static int Additionner(int a, int b) {
+        return (a + b);
+    }
+}
