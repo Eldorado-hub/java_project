c8a0151764aee129cd4d6b6521a8fdb1cedfe277
Eldorado-hub <133639688+Eldorado-hub@users.noreply.github.com>
2023-07-20 14:40:11 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Bonjour, world Devops !");
+        System.out.println("Bonjour, world Devops bbb !");
         Additionner(1, 2);
     }
     public static int Additionner(int a, int b) {
